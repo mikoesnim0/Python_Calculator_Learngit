64d352cd582c6cde7a3ac8a5064350ebff9e08b0
mikoesnim0 <mikoesnim02@gmail.com>
2024-09-14 12:22:37 +0900

    New icon file commit also main.py button widget added

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import sys
-from PyQt5.QtWidgets import QApplication, QWidget
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox)
 
 class Calclator(QWidget):
     def __init__(self):
@@ -7,9 +7,14 @@ class Calclator(QWidget):
         self.initUI()
         
     def initUI(self):
+        self.btn1 = QPushButton("Message", self)
+        self.btn1.clicked.connect(self.activateMessage)
         self.setWindowTitle("Calculator")
         self.resize(2**8, 2**8)
         self.show()
+        
+    def activateMessage(self):
+        QMessageBox.information(self,"information", "Button clicked!")
         
 
 if __name__ =="__main__":
