19f850ad16736bdaeacb8dccfc75b147301fe4ad
mikoesnim0 <mikoesnim02@gmail.com>
2024-09-16 22:21:17 +0900

    New files changed?

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 import sys
-from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox)
+from PyQt5.QtWidgets import (QApplication, QWidget, QPushButton, QVBoxLayout, QMessageBox,
+                             QPlainTextEdit, QHBoxLayout)
 from PyQt5.QtGui import QIcon
 
 class Calclator(QWidget):
@@ -8,15 +9,28 @@ class Calclator(QWidget):
         self.initUI()
         
     def initUI(self):
+        self.te1 = QPlainTextEdit()
+        self.te1.setReadOnly(True)
+        
         self.btn1 = QPushButton("Message", self)
         self.btn1.clicked.connect(self.activateMessage)
+        self.btn2 = QPushButton("clear", self)
+        self.btn2.clicked.connect(self.clearMessage)
+        
+        hbox=QHBoxLayout()
+        hbox.addStretch(1)
+        hbox.addWidget(self.btn1)
+        hbox.addWidget(self.btn2)
         
         vbox=QVBoxLayout()
         vbox.addStretch(1)
-        vbox.addWidget(self.btn1)
+        vbox.addWidget(self.te1)
         vbox.addStretch(1)
+        vbox.addLayout(hbox)
+
         
         self.setLayout(vbox)
+        # self.setLayout(hbox)
         
         self.setWindowTitle("Calculator")
         self.setWindowIcon(QIcon("icon.png"))
@@ -24,7 +38,11 @@ class Calclator(QWidget):
         self.show()
         
     def activateMessage(self):
-        QMessageBox.information(self,"information", "Button clicked!")
+        #QMessageBox.information(self,"information", "Button clicked!")
+        self.te1.appendPlainText("Button clicked")
+        
+    def clearMessage(self):
+        self.te1.clear()
         
         
 
