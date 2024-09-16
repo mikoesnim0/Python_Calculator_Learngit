c7dc967ca0c63141cc6a751dd7dd8036172053c7
mikoesnim0 <mikoesnim02@gmail.com>
2024-09-16 22:52:51 +0900

    QQSocketNotifier error handled, execute normaly

--- main.py
+++ main.py
@@ -4,7 +4,7 @@ from ctrl import Control
 from PyQt5.QtWidgets import QApplication
         
 def main():
-    calc=QApplication(sys.argv)
+    # calc=QApplication(sys.argv)
     app=QApplication(sys.argv)
     view=View()
     Control(view=view)
